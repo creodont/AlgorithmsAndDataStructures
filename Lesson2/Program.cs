d07f531aaa165074fc40989acce75f35f7634e5b
Segj <reflektometer@gmail.com>
2021-04-24 11:35:22 +0300

    Lesson2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -48,7 +48,7 @@ namespace Lesson2
                 else
                 {
                     tail.NextNode = newNode;
-                    newNode.NextNode = tail;
+                    newNode.PrevNode = tail;
                 }
                 tail = newNode;
                 count++;
@@ -121,10 +121,14 @@ namespace Lesson2
                 {
                     if (i == index)
                     {
-                        node.NextNode.PrevNode = node.PrevNode;
-                        node.PrevNode.NextNode = node.NextNode;
+                        
                         if (i == 0) head = node.NextNode;
+                        else node.PrevNode.NextNode = node.NextNode;
+
                         if (i == (count - 1)) tail = node.PrevNode;
+                        else node.NextNode.PrevNode = node.PrevNode;
+
+                        count--;
                         break;
                     }
                     node = node.NextNode;
@@ -137,6 +141,8 @@ namespace Lesson2
 
             public void RemoveNode(Node node)
             {
+                if (node == null) throw new Exception("Ошибка!!! Передан пустой элемент");
+
                 Node sNode = head;
 
                 while (true)
@@ -145,15 +151,40 @@ namespace Lesson2
 
                     if (sNode == node)
                     {
-                        node.NextNode.PrevNode = node.PrevNode;
-                        node.PrevNode.NextNode = node.NextNode;
+                        
                         if (node == head) head = node.NextNode;
+                        else node.PrevNode.NextNode = node.NextNode;
+
                         if (node == tail) tail = node.PrevNode;
+                        else node.NextNode.PrevNode = node.PrevNode;
+
+                        count--;
                         break;
+
                     }
                     sNode = sNode.NextNode;
                 }
 
+            }
+
+            /// <summary>
+            /// Метод вывода значений списка (для отладки) 
+            /// </summary>
+            public void PrintValueList()
+            {
+                Node node = tail;
+                Console.Write("Значения списка (порядок с конца): ");
+                if(node == null)
+                {
+                    Console.Write("пусто");
+                    return;
+                }
+                while (node != null)
+                {
+                    Console.Write($"{node.Value} ");
+                    node = node.PrevNode;
+                }
+                Console.WriteLine();
             }
         }
         
@@ -190,12 +221,80 @@ namespace Lesson2
         {
             // 1. Двусвязный список
             #region case1
+            NodeLinkedList list = new NodeLinkedList();
 
+            Console.WriteLine("Добавить элементы в список");
+            list.AddNode(6);
+            list.AddNode(3);
+            list.AddNode(2);
+            list.PrintValueList();
+            Console.WriteLine($"Количество элеметов в списке = {list.GetCount()}\n");
+
+            Console.WriteLine("Добавить элемент с Value=10 в список после элемента с Value=3");
+            list.AddNodeAfter(list.FindNode(3), 10);
+            list.PrintValueList();
+            Console.WriteLine($"Количество элеметов в списке = {list.GetCount()}\n");
+
+            Console.WriteLine("Удалить элемент с индексом 0");
+            list.RemoveNode(0);
+            list.PrintValueList();
+            Console.WriteLine($"Количество элеметов в списке = {list.GetCount()}\n");
+
+            Console.WriteLine("Удалить элемент с Value=10");
+            list.RemoveNode(list.FindNode(10));
+            list.PrintValueList();
+            Console.WriteLine($"Количество элеметов в списке = {list.GetCount()}\n");
+
+            Console.WriteLine("Удалить пустой элемент");
+            try 
+            {
+                list.RemoveNode(null);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Произошло исключение: {e.Message}\n");
+            }
+
+            Console.WriteLine("Удалить несуществующий элемент");
+            try
+            {
+                list.RemoveNode(new Node { Value = 0});
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Произошло исключение: {e.Message}\n");
+            }
+
+            Console.ReadLine();
             #endregion
 
             // 2. Двоичный поиск
+            // О(log2 n)
             #region case2
 
+            int n = 100;
+            int[] segment = new int[100];
+
+            for (int i = 0; i < n; i++)
+            {
+                segment[i] = i + 5; 
+            }
+
+            Console.WriteLine($"Границы отсортированного массива: Min={segment[0]}, Max={segment[segment.Length-1]}");
+            int searchValue = 25;
+            Console.WriteLine($"Найти индекс числа {searchValue} в массиве: {BinarySearch(segment, searchValue)}");
+            searchValue = 50;
+            Console.WriteLine($"Найти индекс числа {searchValue} в массиве: {BinarySearch(segment, searchValue)}");
+            searchValue = 100;
+            Console.WriteLine($"Найти индекс числа {searchValue} в массиве: {BinarySearch(segment, searchValue)}");
+
+            searchValue = 1;
+            Console.WriteLine($"Найти индекс числа {searchValue} в массиве: {BinarySearch(segment, searchValue)}");
+            searchValue = 107;
+            Console.WriteLine($"Найти индекс числа {searchValue} в массиве: {BinarySearch(segment, searchValue)}");
+            Console.ReadLine();
             #endregion
         }
     }
